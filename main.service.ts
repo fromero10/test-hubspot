29a2bed7ec5b9c958ac0e3e354c4cc196aff13a2
Federico <f.romero10.newman13@gmail.com>
2025-02-01 11:05:55 -0500

    Initial commit

---
+++ main.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+
+  //private baseUrl = 'https://jsonplaceholder.typicode.com'; // Replace with your API URL
+  private baseUrl = 'https://sa-tech-assessment.replit.app/api/assessment/contacts'; // Replace with your API URL
+
+  constructor(private http: HttpClient) {}
+
+  getContacts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  // GET request
+  getPosts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/posts`);
+  }
+
+  // POST request
+  createPost(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/posts`, data);
+  }
+
+  // PUT request
+  updatePost(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/posts/${id}`, data);
+  }
+
+  // DELETE request
+  deletePost(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/posts/${id}`);
+  }
+
+}
