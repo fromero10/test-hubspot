3ee5537efd921d0398ddf581c00ef4f9aae8de6a
Federico <f.romero10.newman13@gmail.com>
2025-02-02 21:44:32 -0500

    Editing the company method

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -14,10 +14,36 @@ export class ContactsComponent implements OnInit {
   company2: Array<any> = [];
   company3: Array<any> = [];
   company: any;
+  companies = [{
+    name: "Company 1",
+    address: "1234 Main St",
+    logo: "https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4",
+    industry: "Tech",
+    description: "This is a description of the tech company",
+    id:1
+  },
+  {
+    name: "Company 2",
+    address: "1234 Secondary street St",
+    logo: "https://fastly.picsum.photos/id/358/2592/1728.jpg?hmac=JXzil51PVU98ffv0R368vfhtSwYa3VZMz51nHnEztyk",
+    industry: "Construction",
+    description: "This is a description of the construction company",
+    id:2
+  },
+  {
+    name: "Company 3",
+    address: "1234 Third St",
+    logo: "https://fastly.picsum.photos/id/43/1280/831.jpg?hmac=glK-rQ0ppFClW-lvjk9FqEWKog07XkOxJf6Xg_cU9LI",
+    industry: "Finance",
+    description: "This is a description of the finance company",
+    id:3
+  },
+  ]
+  companyId!: string | null;
   constructor(private mainService: MainService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.company = this.route.snapshot.paramMap.get('companyId');
+    this.companyId = this.route.snapshot.paramMap.get('companyId');
     this.mainService.getContacts().subscribe(data => {
       let contacts = data.results;
       this.categorizeContacts(contacts);
@@ -25,7 +51,11 @@ export class ContactsComponent implements OnInit {
   }
 
   sendToHubSpot(contact: any) {
-    this.mainService.postHubSpotContacts(contact).subscribe(data => {
+    let object = {
+      contact,
+      company: this.company
+    }
+    this.mainService.postHubSpotContacts(object).subscribe(data => {
       console.log(data);
     })
   }
@@ -46,12 +76,15 @@ export class ContactsComponent implements OnInit {
       }
       
     }
-    if (this.company === '1') {
+    if (this.companyId === '1') {
       this.contacts = this.company1;
-    } else if (this.company === '2') {
+      this.company = this.companies[0];
+    } else if (this.companyId === '2') {
       this.contacts = this.company2;
+      this.company = this.companies[1];
     } else {
       this.contacts = this.company3;
+      this.company = this.companies[2];
     }
   }
 
